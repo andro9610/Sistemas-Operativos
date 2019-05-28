abdb3d6d64a9953c5877b0c28223c4d4ee1fe6b6
andro9610 <andro9610@gmail.com>
2019-05-28 17:09:33 -0500

    Subida del proyecto
    Se sube la totalidad del proyecto, con todas sus clases documentadas.
    
    Pendiente:
    
    * MVC

---
+++ Algoritmos Planificacion/src/Planificador.java
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import javax.swing.JOptionPane;
+
+public class Planificador {
+
+    public Proceso p;
+    public ArrayList<Proceso> listaprocesosA = new ArrayList<Proceso>();
+    public ArrayList<Proceso> listaprocesosB = new ArrayList<Proceso>();
+    
+    Tabla tl = new Tabla();
+
+    /**Inicializar: recibe la cantidad de procesos a calcular, tomando el tiempo
+     * de rafaga de cada uno de ellos
+    */
+    public void inicializar() {
+
+        int pro;
+        pro = Integer.parseInt(JOptionPane.showInputDialog("Inserte la cantidad de procesos"));
+        for (int i = 0; i < pro; i++) {
+            int cont = 1 + i;
+            p = new Proceso();
+            p.setNombre("P"+ cont);
+            p.setLlegada(i);
+            p.setRafaga(Integer.parseInt(JOptionPane.showInputDialog("Ingrese la rafaga del proceso "+p.getNombre())));
+            listaprocesosA.add(p);
+        }
+
+    }
+
+    /**ejecutarFcfs: Tiene el contenido del algoritmo Fcfs*/
+    public void ejecutarFcfs() {
+
+        this.inicializar();
+
+        Iterator it = listaprocesosA.iterator();
+        int time = 0;
+        int contador = 0;
+        while (it.hasNext()) {
+            Proceso p = (Proceso) it.next();
+
+            if (p.getLlegada() == contador) {
+                /**Se realizan los calculos segun las formulas dads en clase*/
+                p.setComienzo(time);
+                p.setFin(p.getComienzo() + p.getRafaga());
+                p.setRetorno(p.getFin() - p.getLlegada());
+                p.setEspera(p.getRetorno() - p.getRafaga());
+                time = p.getFin();
+
+            }
+            contador++;
+        }
+    }
+    /**ejecutarFcfs: Tiene el contenido del algoritmo Sjf*/
+    public void ejecutarSjf() {
+        this.inicializar();
+        Iterator it = listaprocesosA.iterator();
+
+        int time = 0;
+        int contador = 0;
+        for (int i = 0; i < 1; i++) {
+            Proceso p = listaprocesosA.get(i);
+
+            p.setComienzo(time);
+            p.setFin(p.getComienzo() + p.getRafaga());
+            p.setRetorno(p.getFin() - p.getLlegada());
+            p.setEspera(p.getRetorno() - p.getRafaga());
+            time = p.getFin();
+            listaprocesosB.add(p);
+            listaprocesosA.remove(i);
+            
+        }
+
+        
+        
+
+        Collections.sort(listaprocesosA, new Comparator<Proceso>() {
+
+            @Override
+            public int compare(Proceso p1, Proceso p2) {
+                return new Integer(p1.getRafaga()).compareTo(new Integer(p2.getRafaga()));
+            }
+        });
+        for (int i = 0; i < listaprocesosA.size(); i++) {
+             Proceso p = listaprocesosA.get(i);
+
+            p.setComienzo(time);
+            p.setFin(p.getComienzo() + p.getRafaga());
+            p.setRetorno(p.getFin() - p.getLlegada());
+            p.setEspera(p.getRetorno() - p.getRafaga());
+            time = p.getFin();
+            listaprocesosB.add(p);
+            
+            
+
+        }
+        Collections.sort(listaprocesosB, new Comparator<Proceso>() {
+
+            @Override
+            public int compare(Proceso p1, Proceso p2) {
+                return new Integer(p1.getLlegada()).compareTo(new Integer(p2.getLlegada()));
+            }
+        });
+    }
+
+    public void mostrarSiguiente() {
+            Iterator it = listaprocesosA.iterator();
+            while (it.hasNext()) {
+                Proceso p = (Proceso) it.next();
+                tl.Agregar(p.getLlegada(), p.getNombre(), p.getRafaga(), p.getComienzo(), p.getFin(), p.getRetorno(), p.getEspera());
+            }
+            tl.setVisible(true);
+        }
+}
---
+++ Algoritmos Planificacion/src/Proceso.java
@@ -0,0 +1,72 @@
+/**
+ * Clase Proceso: contiene los getters y setters necesarios para que el programa
+ * funcione
+ */
+public class Proceso {
+
+private String Nombre ;
+private int Rafaga ;
+private int Llegada ;
+private int Retorno ;
+private int fin ;
+private int Comienzo ;
+private int Espera ;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getRafaga() {
+        return Rafaga;
+    }
+
+    public void setRafaga(int Rafaga) {
+        this.Rafaga = Rafaga;
+    }
+
+    public int getLlegada() {
+        return Llegada;
+    }
+
+    public void setLlegada(int Llegada) {
+        this.Llegada = Llegada;
+    }
+
+    public int getRetorno() {
+        return Retorno;
+    }
+
+    public void setRetorno(int Retorno) {
+        this.Retorno = Retorno;
+    }
+
+    public int getFin() {
+        return fin;
+    }
+
+    public void setFin(int fin) {
+        this.fin = fin;
+    }
+
+    public int getComienzo() {
+        return Comienzo;
+    }
+
+    public void setComienzo(int Comienzo) {
+        this.Comienzo = Comienzo;
+    }
+
+    public int getEspera() {
+        return Espera;
+    }
+
+    public void setEspera(int Espera) {
+        this.Espera = Espera;
+    }
+
+
+}
